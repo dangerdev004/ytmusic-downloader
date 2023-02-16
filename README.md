06a6e312e8a7ba007861f2e94daff3193479078c
Shreyansh Shrivastava <shreyanshshrivastavatop004@gmail.com>
2023-02-17 00:05:17 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ytmusic-downloader
 Download Music from youtube with metadata
+1. Download the script
+2. Give permissions
+  chmod +x ./musicdownloader.sh
+3. Run the script
+   ./musicdownloader.sh
+4. Enter the name of song or playlist you want to download
+5. Enjoy the music.
---
+++ musicdownloader.sh
@@ -0,0 +1,21 @@
+echo "Enter the youtube link of the song or playlist: "
+read link
+if grep -q "playlist"  <<< "$link" ;
+then 
+ echo "Enter the name of the playlist: "
+ read playname
+ mkdir ~/Music/sources
+ mkdir ~/Music/sources/"$playname"
+ yt-dlp -x -f bestaudio --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" -P /sdcard/Music/sources/"$playname" -o "%(video_autonumber)s-%(title)s.%(ext)s".mp3 $link
+ cd ~/Music/sources/"$playname"
+ ls -1v | grep .mp3 > "$playname".m3u 
+ mkdir ~/Music/"$playname"
+cp *.mp3 ~/Music/"$playname"
+ mv "$playname".m3u ~/Music/"$playname"
+ cd
+ rm -rf ~/Music/sources/
+ else 
+ echo "Enter the name of the song: "
+ read name
+ yt-dlp -x -f bestaudio --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" -P /sdcard/Music -o "$name".mp3 "$link"
+ fi
