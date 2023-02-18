99607cd8b0ee6ee3a6d90ebc585e2b8730dacea1
dangerdev004 <shreyanshshrivastavatop004@gmail.com>
2023-02-18 09:36:11 +0530

    Easy Navigation

---
+++ musicandroid.sh
@@ -0,0 +1,12 @@
+echo "Enter the youtube link of the song or playlist: "
+read link
+if grep -q "playlist"  <<< "$link" ;
+then
+yt-dlp -x -f bestaudio --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" -P /sdcard/Music -o '%(playlist_title)s/%(video_autonumber)s-%(title)s.%(ext)s' "$link"
+output=$(yt-dlp --get-filename -o '%(playlist_title)s' --playlist-start 1 --playlist-end 1 "$link")
+cd /sdcard/Music/"$output"
+ls -1v | grep .mp3 > "$output".m3u
+cd
+else 
+yt-dlp -x -f bestaudio --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" -P /sdcard/Music -o "%(title)s.%(ext)s" "$link"
+fi
